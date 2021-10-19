49c35425c4ef4ec9db13cd7bfaed8aa3a5a68d2b
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-10-19 20:19:19 +0600

    Doctors page complete

--- src/components/Doctors/Doctors.js
+++ src/components/Doctors/Doctors.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Card, Col, Row } from 'react-bootstrap';
 
 const Doctors = () => {
+    const [doctors, setDoctors] = useState([]);
+
+    useEffect(() => {
+        fetch('./db.json')
+            .then(resp => resp.json())
+            .then(data => setDoctors(data.doctors))
+    }, []);
     return (
-        <div>
+        <div className="container">
+            <h1 className="my-5">OUR DOCTORS</h1>
+            <Row xs={1} md={3} lg={3} className="g-5">
+                {doctors.map(data => (
+                    <Col>
+                        <Card className="simple-animation">
+                            <div className="d-flex justify-content-center">
+                                <Card.Img variant="top" src={data.image} className="w-75 py-3" />
+                            </div>
+                            <Card.Body>
+                                <Card.Title>{data.name}</Card.Title>
+                                <Card.Text className="text-muted">
+                                    {data.specialist}
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
 
         </div>
     );
