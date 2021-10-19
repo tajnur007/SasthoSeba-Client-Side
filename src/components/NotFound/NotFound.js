383c92588ca7313d03a22ef9549996b5d120c32c
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-10-20 05:56:27 +0600

    Empty alt attribute added to all img tag

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -13,7 +13,7 @@ const NotFound = () => {
 
     return (
         <div>
-            <div> <img id="image-404" src={image404} alt="404 gif image" /> </div>
+            <div> <img id="image-404" src={image404} alt="" /> </div>
             <h2 className="fw-bold">Oops! Page Not Found</h2>
             <p className="text-muted">The page you were looking for could not be found.</p>
             <div>
