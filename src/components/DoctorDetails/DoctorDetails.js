167034690c55d18cc557ac859c2a30776b23b2aa
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-10-20 22:03:52 +0600

    Doctor details page complete and new data inserted into database

--- src/components/DoctorDetails/DoctorDetails.js
+++ src/components/DoctorDetails/DoctorDetails.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Card, Button } from 'react-bootstrap';
+import { useHistory, useParams } from 'react-router';
 
 const DoctorDetails = () => {
-    return (
-        <div>
+    // All States 
+    const [doctor, setDoctor] = useState([]);
+    const { id } = useParams();
+    const history = useHistory();
 
+    // Data Load 
+    useEffect(() => {
+        fetch('../db.json')
+            .then(resp => resp.json())
+            .then(data => {
+                const doctorObj = data.doctors.find(doctor => doctor.id === id);
+                setDoctor(doctorObj);
+                console.log(doctorObj);
+            })
+    }, []);
+
+    // Data Extraction 
+    const { name, specialist, email, phone, image } = doctor;
+
+    // Jump to All Doctors Page 
+    const viewAllDoctors = () => {
+        history.push('/doctors');
+    }
+
+    return (
+        <div className="container mt-5 d-flex justify-content-center">
+            <Card className="col-10 col-sm-8 col-md-6 col-lg-5 col-xl-4">
+                <h2 className="pt-3 fw-bold">{name}</h2>
+                <div className="d-flex justify-content-center">
+                    <Card.Img variant="top" src={image} className="w-75 py-3" />
+                </div>
+                <h4 className="text-muted fw-bold">{specialist}</h4>
+                <p>
+                    Email: {email} <br />
+                    Phone: {phone}
+                </p>
+                <Button onClick={viewAllDoctors} variant="primary">View All Doctors</Button>
+            </Card>
         </div>
     );
 };
