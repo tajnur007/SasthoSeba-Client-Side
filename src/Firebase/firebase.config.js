555031c36b8bcb05b11000bd44728d71998bc902
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-10-20 12:22:25 +0600

    Firebase initialization method completed

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCdWEqgbhMKbA1t8Sr9w8Q0lRf8I79-FzI",
+    authDomain: "sastho-seba-bd.firebaseapp.com",
+    projectId: "sastho-seba-bd",
+    storageBucket: "sastho-seba-bd.appspot.com",
+    messagingSenderId: "119011161439",
+    appId: "1:119011161439:web:ef8f92b9fccbbb72c7f2ea"
+};
+
+export default firebaseConfig;
